cd06663b7898b6ee4b2a4c3fef0cec5a02098333
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-05 15:05:32 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Created by Graeme
+// Created on: May 5
+// JS functions for index.html
+
+
+// This function tells the user the cost of getting into the museum
+'use strict';
+function submit() {
+	// Declares variable
+	let userAge = document.getElementById('age').value
+	let day = select.options[select.selectedIndex].value
+	
+	// If statement for user age, what age category of movies they can see
+	if (userAge == "") {
+		document.getElementById('cost').innerHTML = "You need to input your age";
+	} else if (userAge )
+		
+}
