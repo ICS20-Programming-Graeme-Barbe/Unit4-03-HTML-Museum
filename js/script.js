760ecc004db8492031dbe0896e244dc339e7db1e
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-06 01:50:33 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -5,14 +5,20 @@
 
 // This function tells the user the cost of getting into the museum
 'use strict';
-function submit() {
+function enter() {
 	// Declares variable
-	let userAge = document.getElementById('age').value
+	let userAge = parseInt(document.getElementById('age').value)
+	let select = document.getElementById('day')
 	let day = select.options[select.selectedIndex].value
 	
 	// If statement for user age, what age category of movies they can see
-	if (userAge == "") {
-		document.getElementById('cost').innerHTML = "You need to input your age";
-	} else if (userAge )
-		
+	if (userAge == "" || day == "") {
+		document.getElementById('cost').innerHTML = "You need to input your age or select the day";
+	} else if (userAge <= 5 || userAge >= 95) {
+		document.getElementById('cost').innerHTML = "Congratulations, you get in for free!"
+	} else if ((userAge >= 12 && userAge <= 21) || (day == "Tuesday" || day == "Thursday")) {
+		document.getElementById('cost').innerHTML = "Congratulations, you get student pricing"
+	} else {
+		document.getElementById('cost').innerHTML = "You need to pay full price."
+	}
 }
